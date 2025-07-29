4c68450da8f5646e2ddff0b6927660d95f16fa79
Steopa2001 <moraru495@gmail.com>
2025-07-29 15:40:50 +0200

    feat (names.js): create and exports a function

---
+++ names/names.js
@@ -0,0 +1,11 @@
+
+//Creo la funzione con due parametri
+function nomi (firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+};
+
+//Esporto la funzione
+module.exports = nomi;
