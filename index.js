335c37e7a094e3a7d91fd250a9a2fb6e3b9c2359
Steopa2001 <moraru495@gmail.com>
2025-07-29 16:26:23 +0200

    feat (people.js): call the functions with the asked parameters

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ console.log(hobbies('play football', 'sleep', 'play tennis'));
 //Creo funzione che non ha parametri
 //Mi restituisce un oggetto con due proprietà: fullName ed hobbies
 function getNamesAndHobbiesInfo (){
+// Chiamo le funzioni con i parametri richiesti
+    const fullName = nomi('Giuseppe', 'Verdi');
+    const hobbiesList = hobbies('play football', 'sleep', 'play tennis');
 
+   
 };
 
 console.log(getNamesAndHobbiesInfo())
